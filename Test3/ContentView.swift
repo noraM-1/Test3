0a453bb00e7fad3d17c8c5fdffec001a865f0ea1
Nora Martin <norakathleenmartin@gmail.com>
2025-07-09 18:58:01 -0400

    Initial Commit

---
+++ Test3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test3
+//
+//  Created by Amanda Martin on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test3/Test3App.swift
@@ -0,0 +1,17 @@
+//
+//  Test3App.swift
+//  Test3
+//
+//  Created by Amanda Martin on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
